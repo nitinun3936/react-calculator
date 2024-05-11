0d11faa7e8fc68dee8001c523487d71a49ce8aac
Nithin <nithin@Nithins-MacBook-Pro.local>
2024-05-11 14:08:13 +0530

    Refactor calculator layout and logic

--- src/CalculatorComponent.js
+++ src/CalculatorComponent.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 const CalculatorComponent = ({ handleClick }) => {
   const buttons = [
-    '7', '8', '9', '/', 
-    '4', '5', '6', '*', 
-    '1', '2', '3', '-', 
-    'C', '0', '=', '+'
+    ['7', '8', '9', '+'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '*'],
+    ['C', '0', '=', '/']
   ];
 
   return (
     <div>
-      {buttons.map((button, idx) => (
-        <button key={idx} onClick={() => handleClick(button)} className="button">
-          {button}
-        </button>
+      {buttons.map((row, idx) => (
+        <div key={idx} className="button-row">
+          {row.map((button) => (
+            <button key={button} onClick={() => handleClick(button)} className="button">
+              {button}
+            </button>
+          ))}
+        </div>
       ))}
     </div>
   );
